7bc8dcb87db58f2212f86cb5dfd7a9fead5167b0
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-30 15:07:33 +0530

    remove message

---
+++ Hiru_M3_API_SQS/getRemove.js
@@ -0,0 +1,24 @@
+let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.receiveAndDeleteMessages({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '3',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		console.log(receivedMessages);
+		return receivedMessages;
+	}, function (deleteSuccessData) {
+		// implement delete success state here
+		console.log(deleteSuccessData);
+	}, function (error) {
+		// implement error handling logic here
+		console.log(error);
+	});
+
+
+	callback(null, 'Successfully executed');
+}
